4b5609a3008dedc7c7022b2c8e305c2f3b30a45e
Levin Mathew Abraham <levinabraham2819@gmail.com>
2025-06-23 20:12:36 +0400

    updated
    -updated navbar links
    -improved benefits carousel
    -improved sectors section
    -fixed header and footer links
    -fixed responsiveness for various elements in the webpage

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Enable dropdown on hover for desktop
 document.addEventListener('DOMContentLoaded', function () {
     // Only apply on screens wider than 768px (md and up)
     if (window.innerWidth > 700) {
-      document.querySelectorAll('.navbar .dropdown').forEach(function (dropdown) {
+      document.querySelectorAll('.dropdown').forEach(function (dropdown) {
         dropdown.addEventListener('mouseenter', function () {
           let dropdownToggle = this.querySelector('.dropdown-toggle');
           if (dropdownToggle) {
@@ -21,33 +20,54 @@ document.addEventListener('DOMContentLoaded', function () {
         });
       });
     }
-});
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+//     const viewMoreBtn = document.getElementById('view-more-sectors');
+//     const viewLessBtn = document.getElementById('view-less-sectors');
+//     const sectorsGrid = document.getElementById('sectors-grid');
+//     const hiddenSectors = sectorsGrid.querySelectorAll('.col.d-none');
+
+//     if(viewMoreBtn) {
+//       viewMoreBtn.addEventListener('click', function() {
+//         hiddenSectors.forEach(function(card) {
+//           card.classList.remove('d-none');
+//         });
+//         viewMoreBtn.classList.add('d-none');
+//         viewLessBtn.classList.remove('d-none');
+//       });
+//     }
+
+//     if(viewLessBtn) {
+//       viewLessBtn.addEventListener('click', function() {
+//         hiddenSectors.forEach(function(card) {
+//           card.classList.add('d-none');
+//         });
+//         viewLessBtn.classList.add('d-none');
+//         viewMoreBtn.classList.remove('d-none');
+//       });
+//     }
 });
 
 $('.owl-carousel').owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:false,
-            loop:false
-        }
-    }
+  items:4,
+  loop:true,
+  margin:10,
+  autoplay:true,
+  autoplayTimeout:2000,
+  autoplayHoverPause:true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:false
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:4,
+          nav:false,
+          loop:false
+      }
+  }
 });
